10c4ea3dcd3c06ffa960e46a57696dee45aa64a6
Saumya Patel <saumya.patel@aimsinfosoft.com>
2022-04-11 19:23:12 +0530

    checkOut page Implemetation thank you page design

--- src/components/ThankYou/ThankYou.js
+++ src/components/ThankYou/ThankYou.js
@@ -5,26 +5,26 @@ import { useNavigate } from "react-router-dom";
 const ThankYou = () => {
     const handleClick = (e) => {
         navigate("/");
-      };
+    };
 
-      const navigate = useNavigate();
+    const navigate = useNavigate();
 
     return (
         <div className="thankyoucontent">
-        <div className="wrapper-1">
-           <div className="wrapper-2">
-            <h1>Thank you!</h1>
-             <button className="go-home" onClick={handleClick}>
-               home page
-             </button>
-           </div>
-       </div>
-        
-       
-       
-       
-       
-      </div>
+            <div className="wrapper-1">
+                <div className="wrapper-2">
+                    <h1>Thank you!</h1>
+                    <button className="go-home" onClick={handleClick}>
+                        home page
+                    </button>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
     )
 };
 
